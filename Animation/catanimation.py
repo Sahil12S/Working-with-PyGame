a854eb0c1dcdfc4c6358783d7c52fe7142cead9c
Sahil Sharma <sahil.sharma@sjsu.edu>
2018-09-01 16:25:11 -0700

    add comments

--- Animation/catanimation.py
+++ Animation/catanimation.py
@@ -15,7 +15,7 @@ DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
 pygame.display.set_caption('Animation')
 
 WHITE = (255, 255, 255)     # set background color
-catImg = pygame.image.load('cat.png')   # load the image
+catImg = pygame.image.load('./cat.png')   # load the image
 catx = 10
 caty = 10
 direction = 'right'
